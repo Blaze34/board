3750d472bf7b37a129a1b5edeeb6c47c5b82fd8b
Blaze34 <30avenyamadeo@gmail.com>
2014-04-01 10:50:13 +0300

    skill list helper

--- app/helpers/my_bootstrap_helper.rb
+++ app/helpers/my_bootstrap_helper.rb
@@ -6,12 +6,20 @@ module MyBootstrapHelper
       content_tag(
         :div,
         content_tag(:button, raw('&times;'), class: 'close', 'data-dismiss' => 'alert') +
-        msg.html_safe, :class => 'alert fade in alert-error'
+        msg.html_safe, class: 'alert fade in alert-error'
       )
     end
 
     errors.join.html_safe
+  end
 
-    #html.html_safe
+  def skills_list(list)
+    return '' if list.blank?
+
+    tags = list.split(',').reject { |t| t.blank? } .map do |t|
+      content_tag(:span, t, class: 'skill_tag btn-link')
+    end
+
+    tags.join(', ').html_safe
   end
 end
